6060078b31d266ba5dd62fdf7f5c180c4aa678f1
Matteo <125mamatucci@frhsd.com>
2023-11-14 07:40:14 -0500

    Uploaded to Github

---
+++ js/ball.js
@@ -0,0 +1,8 @@
+let userName = "Matteo";
+
+userName ? console.log("Hello, Matteo!") : console.log("Hello!");
+
+let userQuestion = "Will I be rich?";
+console.log(userQuestion)
+
+let randomNumber =  
