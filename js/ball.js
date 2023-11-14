1a0fc74e21097d9a07bbcf0e34db10691d6d7552
Matteo <125mamatucci@frhsd.com>
2023-11-14 08:21:33 -0500

    finished random eight ball answers

--- js/ball.js
+++ js/ball.js
@@ -5,4 +5,33 @@ userName ? console.log("Hello, Matteo!") : console.log("Hello!");
 let userQuestion = "Will I be rich?";
 console.log(userQuestion)
 
-let randomNumber =  
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    console.log(eightBall = "It is certain"); 
+    break;
+  case 1:
+    console.log(eightBall = "It is decidedly so"); 
+    break;
+  case 2:
+    console.log(eightBall = "Reply hazy try again"); 
+    break;
+  case 3:
+    console.log(eightBall = "Cannot predict now"); 
+    break;
+  case 4:
+    console.log(eightBall = "Do not count on it"); 
+    break;
+  case 5:
+    console.log(eightBall = "My sources say no"); 
+    break;
+  case 6:
+    console.log(eightBall = "Outlook not so good"); 
+    break;
+  case 7:
+    console.log(eightBall = "Signs point to yes"); 
+    break;
+}
